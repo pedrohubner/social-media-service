bbc0c346b08edef899aee15578b06f5bb05bc7ab
pedrohubner <pedronhubner@gmail.com>
2022-02-03 17:43:21 -0300

    Commenting classes.

--- src/main/java/com/socialmedia/common/utils/Properties.java
+++ src/main/java/com/socialmedia/common/utils/Properties.java
@@ -1,14 +1,16 @@
-package com.socialmedia.common.utils;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-
-@Getter
-@AllArgsConstructor
-public class Properties {
-    @Value("${error.message.post-not-found}")
-    private String postNotFoundMessage;
-    @Value("${error.suggestion.post-not-found}")
-    private String postNotFoundSuggestion;
-}
+//package com.socialmedia.common.utils;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Getter
+//@Component
+//@AllArgsConstructor
+//public class Properties {
+//    @Value("${error.message.post-not-found}")
+//    private String postNotFoundMessage;
+//    @Value("${error.suggestion.post-not-found}")
+//    private String postNotFoundSuggestion;
+//}
